6c6687f194daa5e3fa598456e7cf693f8d22317d
Mayank Bansal <mayankbansal125@gmail.com>
2025-11-03 21:06:00 +0530

    fix: styling for form header

--- app/components/new-join-steps/stepper-header.js
+++ app/components/new-join-steps/stepper-header.js
@@ -1,7 +1,14 @@
 import Component from '@glimmer/component';
+import { htmlSafe } from '@ember/template';
 
 export default class StepperHeaderComponent extends Component {
   get progressPercentage() {
-    return Math.round((this.args.currentStep / this.args.totalSteps) * 100);
+    const totalSteps = +this.args.totalSteps ?? 1;
+    const currentStep = +this.args.currentStep ?? 0;
+    return Math.round((currentStep / totalSteps) * 100);
+  }
+
+  get progressStyle() {
+    return htmlSafe(`width: ${this.progressPercentage}%`);
   }
 }
