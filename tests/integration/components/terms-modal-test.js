ace4d9b4de1ed81fea7d68933a2cd557fc577b09
Mayank Bansal <mayankbansal125@gmail.com>
2025-11-08 01:27:53 +0530

    feat: add new stepper and welcome screen for form (#1081)
    * feat: add new stepper and welcome screen for form
    
    * refactor: remove redudant css, simplify steps, modal flow
    
    * fix: add oldOnboarding param for test
    
    * refactor: css naming and remove redundant styles
    
    * refactor: use application terms service as shared state
    
    * test: add test for new stepper and welcome screen
    
    * fix: use reusable es iicon button for close modal
    
    * fix: currentStep initialization and new stepper constructor
    
    * fix: rterms modal checkbox validation and improve naming
    
    * fix: fix bullet point and add test for oldOnboarding
    
    * refactor: naming and copy for terms, move css to next PR
    
    * refactor: naming for application terms service
    
    * refactor: remove unused id in localStorage
    
    * refactor: update storage handling in new stepper
    
    * fix: update color shade for text and naming for welcome screen
    
    * refactor: update query param step in new stepper

---
+++ tests/integration/components/terms-modal-test.js
@@ -0,0 +1,70 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'website-www/tests/helpers';
+import { render, click } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | terms-modal', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders modal content when @isOpen is true', async function (assert) {
+    this.setProperties({
+      isOpen: true,
+      onAccept: () => {},
+      onClose: () => {},
+    });
+    await render(hbs`
+      <TermsModal
+        @isOpen={{this.isOpen}}
+        @onAccept={{this.onAccept}}
+        @onClose={{this.onClose}}
+      />
+    `);
+    assert
+      .dom('[data-test="terms-modal-content"]')
+      .exists('Modal content is displayed');
+  });
+
+  test('it shows Accept Terms button and correctly calls onAccept action', async function (assert) {
+    assert.expect(3);
+    const terms = this.owner.lookup('service:joinApplicationTerms');
+    terms.hasUserAcceptedTerms = false;
+    this.setProperties({
+      isOpen: true,
+      onAccept: () => assert.step('accept-called'),
+      onClose: () => {},
+    });
+    await render(hbs`
+      <TermsModal
+        @isOpen={{this.isOpen}}
+        @onAccept={{this.onAccept}}
+        @onClose={{this.onClose}}
+      />
+    `);
+    assert
+      .dom('[data-test-button="accept-terms"]')
+      .exists('Accept Terms button is displayed');
+
+    await click('[data-test-button="accept-terms"]');
+    assert.verifySteps(['accept-called'], 'onAccept action was called');
+  });
+
+  test('it does not show Accept Terms button when terms are already accepted', async function (assert) {
+    const terms = this.owner.lookup('service:joinApplicationTerms');
+    terms.hasUserAcceptedTerms = true;
+    this.setProperties({
+      isOpen: true,
+      onAccept: () => {},
+      onClose: () => {},
+    });
+    await render(hbs`
+      <TermsModal
+        @isOpen={{this.isOpen}}
+        @onAccept={{this.onAccept}}
+        @onClose={{this.onClose}}
+      />
+    `);
+    assert
+      .dom('[data-test-button="accept-terms"]')
+      .doesNotExist('Accept Terms button is not displayed');
+  });
+});
