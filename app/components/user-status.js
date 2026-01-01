e38c35a794792b75c4d372feed0036aa0b7f867b
Dhirenderchoudhary <dhirenderchoudhary0001@gmailcom>
2026-01-01 21:49:26 +0530

    message changed

--- app/components/user-status.js
+++ app/components/user-status.js
@@ -20,7 +20,7 @@ export default class UserStatusComponent extends Component {
     },
     [USER_STATES.OOO]: {
       status: USER_STATES.OOO,
-      message: 'Request OOO Status',
+      message: 'Request OOO',
       class: 'buttons__ooo',
     },
   };
